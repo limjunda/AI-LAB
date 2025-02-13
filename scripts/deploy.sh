c6d45390cb65478125a121f2b40f72b03af427f7
limjunda <limjunda@hotmail.com>
2025-02-13 13:21:33 +0800

    amend index html file for title page, added deploy script for google cloud run, adjust image for header

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-cd $(dirname $0)/..
+# Build Docker Image
+docker build --no-cache -t ai-solution .
 
-export PROJECT_ID=prusandbx-nprd-uat-rt5044
-export CLOUDRUN_SERVICE_NAME=ailab-solution-taxonomy-frontend
-export CLOUDRUN_SERVICE_IMAGE_NAME=ai-solution-taxonomy/$PROJECT_ID/$CLOUDRUN_SERVICE_NAME
+# Tag Docker Image
+docker tag ai-solution asisa-southeast1-docker.pkg.dev/prusandbx-nprd-uat-rt5044/ailab-solution-taxonomy/ai-solution:latest
 
-gcloud config set project $PROJECT_ID
-gcloud auth application-default set-quota-project $PROJECT_ID
+# Push Docker Image to Artifact Registry
+docker push asia-southeast1-docker.pkg.dev/prusandbx-nprd-uat-rt5044/ailab-solution-taxonomy/ai-solution:latest
 
-export REPO_FULL_NAME=$CLOUDRUN_SERVICE_IMAGE_NAME
-gcloud builds submit \
-  --config cloudbuild.yaml \
-  --substitutions=REPO_FULL_NAME=$REPO_FULL_NAME .
-
-gcloud run deploy $CLOUDRUN_SERVICE_NAME \
-  --image $CLOUDRUN_SERVICE_IMAGE_NAME \
-  --region asia-southeast1 \
-  --port 3000 --allow-unauthenticated
+# Deploy to Cloud Run
+gcloud run deploy ailab-solution-taxonomy --image asia-southeast1-docker.pkg.dev/prusandbx-nprd-uat-rt5044/ailab-solution-taxonomy/ai-solution:latest --platform managed --region asia-southeast1
