f31a6b33ab2d4d886721f3485667994316106b85
limjunda <limjunda@hotmail.com>
2025-02-20 06:31:17 +0000

    Update package and deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@
 docker build --no-cache -t ai-solution .
 
 # Tag Docker Image
-docker tag ai-solution asisa-southeast1-docker.pkg.dev/prusandbx-nprd-uat-rt5044/ailab-solution-taxonomy/ai-solution:latest
+docker tag ai-solution asia-southeast1-docker.pkg.dev/prusandbx-nprd-uat-rt5044/ailab-solution-taxonomy/ai-solution:latest
 
 # Push Docker Image to Artifact Registry
 docker push asia-southeast1-docker.pkg.dev/prusandbx-nprd-uat-rt5044/ailab-solution-taxonomy/ai-solution:latest
